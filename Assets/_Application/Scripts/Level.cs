f14a1f35383c56a373865d75471aeeb92436ef13
Lukas Niemeier <lukas.niemeier@koeln.de>
2020-07-11 12:22:23 +0200

    Playing with Unity Test Runner

---
+++ Assets/_Application/Scripts/Level.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using UnityEngine;
+
+namespace GMTK2020
+{
+    public class Level
+    {
+        public Tile[,] Grid { get; }
+
+        public Level(Tile[,] grid)
+        {
+            Grid = grid;
+        }
+
+        public void PrintToConsole()
+        {
+            var message = new StringBuilder();
+            for (int x = 0; x < Grid.GetLength(0); x++)
+            {
+                for (int y = 0; y < Grid.GetLength(1); y++)
+                {
+                    message.Append(Grid[x, y].Color + "\t");
+                }
+                message.AppendLine("");
+            }
+            Debug.Log(message.ToString());
+        }
+    }
+
+    public class Tile
+    {
+        public string Color { get; }
+
+        public Tile(string color)
+        {
+            Color = color;
+        }
+    }
+
+}
