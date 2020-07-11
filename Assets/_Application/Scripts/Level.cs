157b2a2ddd8ffa9142013051e2c7f661fd734989
Lukas Niemeier <lukas.niemeier@koeln.de>
2020-07-11 12:50:10 +0200

    Drafting components

--- Assets/_Application/Scripts/Level.cs
+++ Assets/_Application/Scripts/Level.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
+using UnityEditorInternal;
 using UnityEngine;
 
 namespace GMTK2020
@@ -26,6 +28,103 @@ namespace GMTK2020
             }
             Debug.Log(message.ToString());
         }
+    }
+
+    public class Main
+    {
+
+        private Tile[,] GenerateLevel()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Prediction GetPredictionsFromHumansBrain()
+        {
+            throw new NotImplementedException();
+        }
+
+        private BetterLevel GenerateValidLevel(Simulator simulator)
+        {
+            // TODO: find legal simulation
+            Tile[,] grid = GenerateLevel();
+            Simulation simulation = simulator.Simulate(grid);
+            return new BetterLevel(grid, simulation);
+        }
+
+        public void runIt()
+        {
+            Simulator simulator = new Simulator();
+            Renderer renderer = new Renderer();
+
+
+            BetterLevel level = GenerateValidLevel();
+
+            renderer.RenderInitial(grid);
+
+            // It's the Human's turn.
+            Validator validator = new Validator();
+            Prediction prediction = GetPredictionsFromHumansBrain();
+            int correctSteps = validator.ValidatePrediction(simulation, prediction);
+
+            renderer.RenderSimulation(simulation, correctSteps);
+        }
+    }
+
+    public class BetterLevel
+    {
+
+        public Tile[,] Grid { get; }
+        public Simulation Simulation { get; }
+        public BetterLevel(Tile[,] grid, Simulation simulation)
+        {
+            Grid = grid;
+            Simulation = simulation;
+        }
+    }
+
+    public class Renderer
+    {
+        public void RenderInitial(Tile[,] grid)
+        {
+
+        }
+
+        public void RenderSimulation(Simulation simulation, int correctPredictions)
+        {
+
+        }
+    }
+
+    public class Simulator
+    {
+        public Simulation Simulate(Tile[,] grid)
+        {
+            return null;
+        }
+    }
+
+    public class Validator
+    {
+        public int ValidatePrediction(Simulation simulation, Prediction prediction)
+        {
+            return 5;
+        }
+    }
+
+    public class Prediction
+    {
+        public List<HashSet<Tile>> MatchedTilesPerStep { get; set; }
+    }
+
+    public class Simulation
+    {
+        public List<SimulationStep> Steps { get; }
+    }
+
+    public class SimulationStep
+    {
+        public HashSet<Tile> MatchedTiles { get; }
+        public List<(Tile, Vector2Int)> MovingTiles { get; }
     }
 
     public class Tile
