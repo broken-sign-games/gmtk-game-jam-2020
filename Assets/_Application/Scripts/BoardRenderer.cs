24dee97072ebbf2330a21670862ef22be88d77bc
Martin Ender <m.buettner.d@gmail.com>
2020-07-11 16:20:05 +0200

    Render initial grid

---
+++ Assets/_Application/Scripts/BoardRenderer.cs
@@ -0,0 +1,34 @@
+﻿using GMTK2020.Data;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GMTK2020
+{
+    public class BoardRenderer : MonoBehaviour
+    {
+        [SerializeField] private TileData tileData;
+
+        private Dictionary<Tile, SpriteRenderer> tileDictionary = new Dictionary<Tile, SpriteRenderer>();
+
+        public void RenderInitial(Tile[,] grid)
+        {
+            int width = grid.GetLength(0);
+            int height = grid.GetLength(1);
+
+            for (int x = 0; x < width; ++x)
+                for (int y = 0; y < height; ++y)
+                {
+                    Tile tile = grid[x, y];
+                    SpriteRenderer tileRenderer = Instantiate(tileData.PrefabMap[tile.Color], transform);
+
+                    tileRenderer.transform.localPosition = new Vector3(x, y, 0);
+                    tileDictionary[tile] = tileRenderer;
+                }
+        }
+
+        public void RenderSimulation(Simulation simulation, int correctPredictions)
+        {
+
+        }
+    } 
+}
---
+++ Assets/_Application/Scripts/Data/TileData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace GMTK2020.Data
+{
+    [CreateAssetMenu]
+    public class TileData : ScriptableObject
+    {
+        public SpriteRenderer[] PrefabMap;
+    }
+}
