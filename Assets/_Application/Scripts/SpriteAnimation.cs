a1f7ea0006807318cb893c3cc014a2af88710144
Lukas Niemeier <lukas.niemeier@koeln.de>
2020-07-12 18:18:32 +0200

    Add in-game logo

--- Assets/_Application/Scripts/SpriteAnimation.cs
+++ Assets/_Application/Scripts/SpriteAnimation.cs
@@ -1,10 +1,15 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using TMPro;
+using UnityEngine;
 
 [RequireComponent(typeof(SpriteRenderer))]
 public class SpriteAnimation : MonoBehaviour
 {
     [SerializeField] private Sprite[] frames = null;
+    [SerializeField] private Sprite finalFrame = null;
     [SerializeField] private int framesPerSecond = 20;
+
+    private bool isIntro = true;
 
     SpriteRenderer splashRenderer;
 
@@ -15,12 +20,24 @@ public class SpriteAnimation : MonoBehaviour
 
     private void Update()
     {
-        var index = (int)(Time.time * framesPerSecond) % frames.Length;
-        var currentFrame = splashRenderer.sprite;
-        if (currentFrame == frames[frames.Length - 1])
+        if (!isIntro)
         {
             return;
         }
+
+        var index = (int)(Time.time * framesPerSecond) % frames.Length;
+        var currentFrame = splashRenderer.sprite;
         splashRenderer.sprite = frames[index];
+
+        if (currentFrame == frames[frames.Length - 1])
+        {
+            isIntro = false;
+            splashRenderer.DOFade(0.0f, 1.0f).OnComplete(() =>
+            {
+                splashRenderer.sprite = finalFrame;
+                splashRenderer.DOFade(1.0f, 0.25f);
+            });
+            return;
+        }
     }
 }
