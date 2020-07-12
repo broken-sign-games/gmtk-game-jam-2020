f1dba15d11185da25f00213456649f2c4a4fcada
Lukas Niemeier <lukas.niemeier@koeln.de>
2020-07-12 11:33:43 +0200

    Exit per ESC

---
+++ Assets/_Application/Scripts/UI/Exit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
