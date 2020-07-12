0c789edc301e291ff2d58e1ca91f7838de31f223
Lukas Niemeier <lukas.niemeier@koeln.de>
2020-07-12 11:10:06 +0200

    Add some example background music

---
+++ Assets/_Application/Scripts/Jukebox.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Jukebox : MonoBehaviour
+{
+
+    [SerializeField] private AudioClip[] Soundtrack;
+
+    private AudioSource AudioSource = null;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Start()
+    {
+        this.AudioSource = GetComponent<AudioSource>();
+
+        if (!AudioSource.playOnAwake)
+        {
+            AudioSource.clip = Soundtrack[Random.Range(0, Soundtrack.Length)];
+            AudioSource.Play();
+        }
+    }
+
+    void Update()
+    {
+        if (!AudioSource.isPlaying)
+        {
+            AudioSource.clip = Soundtrack[Random.Range(0, Soundtrack.Length)];
+            AudioSource.Play();
+        }
+    }
+
+}
