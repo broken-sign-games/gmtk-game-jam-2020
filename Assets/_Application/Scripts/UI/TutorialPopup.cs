c55a24c6a053c11df63b7b4a5c20a643893667b9
Martin Ender <m.buettner.d@gmail.com>
2020-08-04 00:10:44 +0200

    Set up basic UI for tutorial message

---
+++ Assets/_Application/Scripts/UI/TutorialPopup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace GMTK2020.UI
+{
+    public class TutorialPopup : MonoBehaviour
+    {
+        [SerializeField] TextMeshProUGUI tutorialText = null;
+
+        public event Action Dismissed;
+
+        public void Dismiss()
+        {
+            gameObject.SetActive(false);
+            Dismissed?.Invoke();
+        }
+    }
+}
