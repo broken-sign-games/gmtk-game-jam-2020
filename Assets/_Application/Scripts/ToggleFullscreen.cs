885cd7a5b8477ba807dd4acf1d4735c3a428fe29
Martin Ender <m.buettner.d@gmail.com>
2020-08-13 10:09:36 +0200

    Implement better fullscreen toggle

---
+++ Assets/_Application/Scripts/ToggleFullscreen.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace GMTK2020
+{
+    public class ToggleFullscreen : MonoBehaviour
+    {
+        private Resolution nativeResolution;
+
+        private bool isFullscreen;
+        private bool wasFullscreen;
+
+        private void Start()
+        {
+            nativeResolution = Screen.resolutions.OrderByDescending(r => r.width).First();
+
+            isFullscreen = wasFullscreen = Screen.fullScreen;
+
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private bool fixSize = false;
+        private void Update()
+        {
+            isFullscreen = Screen.fullScreen;
+
+            if (isFullscreen && !wasFullscreen)
+            {
+                Screen.SetResolution(nativeResolution.width, nativeResolution.height, FullScreenMode.FullScreenWindow);
+            }
+            else if (!isFullscreen && wasFullscreen)
+            {
+                Screen.SetResolution(960, 540, FullScreenMode.Windowed);
+            }
+
+            wasFullscreen = isFullscreen;
+        }
+    } 
+}
