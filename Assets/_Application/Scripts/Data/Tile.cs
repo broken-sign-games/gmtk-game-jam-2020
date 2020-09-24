e8123f512f3f04d9799506ba6bd056d957a0a02d
Martin Ender <m.buettner.d@gmail.com>
2020-09-24 21:37:05 +0200

    Minor additions to Tile class

--- Assets/_Application/Scripts/Data/Tile.cs
+++ Assets/_Application/Scripts/Data/Tile.cs
@@ -4,9 +4,18 @@
     {
         public int Color { get; }
 
+        public bool Inert { get; private set; }
+        public bool Marked { get; set; }
+
         public Tile(int color)
         {
             Color = color;
+        }
+
+        public void MakeInert()
+        {
+            Inert = true;
+            Marked = false;
         }
     }
 }
