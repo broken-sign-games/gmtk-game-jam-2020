26fb854a4f9ac5a751e890de991077f21f3c2069
Martin Ender <m.buettner.d@gmail.com>
2020-10-10 10:54:57 +0200

    Tweak cork animation

--- Assets/_Application/Scripts/Rendering/TileRenderer.cs
+++ Assets/_Application/Scripts/Rendering/TileRenderer.cs
@@ -6,7 +6,8 @@ namespace GMTK2020.Rendering
 {
     public class TileRenderer : MonoBehaviour
     {
-        [SerializeField] private SpriteRenderer vialSprite = null;
+        [SerializeField] private Transform vialTransform = null;
+        [SerializeField] private SpriteRenderer glassSprite = null;
         [SerializeField] private SpriteRenderer liquidSprite = null;
         [SerializeField] private SpriteRenderer corkSprite = null;
         [SerializeField] private SpriteMask liquidMask = null;
@@ -16,7 +17,8 @@ namespace GMTK2020.Rendering
         [SerializeField] private ParticleSystem bubbles = null;
         [SerializeField] private float tileFadeDuration = 0.25f;
         [SerializeField] private float corkDistance = 0.1f;
-        [SerializeField] private float corkSpeed = 0.5f;
+        [SerializeField] private float corkMoveDuration = 0.5f;
+        [SerializeField] private float corkFadeDuration = 0.5f;
         [SerializeField] private float tiltFrequency = 1f;
         [SerializeField] private float tiltAmplitude = 10f;
         [SerializeField] private TileData tileData = null;
@@ -28,20 +30,21 @@ namespace GMTK2020.Rendering
             if (!tile.Marked)
                 return;
 
-            transform.localRotation = Quaternion.Euler(0, 0, Mathf.Sin(Time.time * tiltFrequency) * tiltAmplitude);
+            vialTransform.localRotation = Quaternion.Euler(0, 0, Mathf.Sin(Time.time * tiltFrequency) * tiltAmplitude);
         }
 
         public void SetTile(Tile tile)
         {
             this.tile = tile;
 
-            UpdatePrediction();
             transform.localPosition = (Vector2)tile.Position;
 
-            vialSprite.sprite = tileData.VialSpriteMap[tile.Color];
+            glassSprite.sprite = tileData.VialSpriteMap[tile.Color];
             corkSprite.sprite = tileData.CorkSpriteMap[tile.Color];
             liquidSprite.sprite = tileData.LiquidSpriteMap[tile.Color];
             liquidMask.sprite = tileData.LiquidSpriteMap[tile.Color];
+
+            UpdatePrediction();
         }
 
         public void UpdatePrediction()
@@ -49,21 +52,27 @@ namespace GMTK2020.Rendering
             if (tile.Marked)
             {
                 bubbles.Play();
-                corkSprite.transform.DOLocalMoveY(corkDistance, corkSpeed).SetSpeedBased().OnComplete(() => corkSprite.enabled = false);
+                DOTween.Complete(corkSprite);
+                Sequence seq = DOTween.Sequence().SetId(corkSprite);
+                seq.Append(corkSprite.transform.DOLocalMoveY(corkDistance, corkMoveDuration));
+                seq.Append(corkSprite.DOFade(0, corkFadeDuration));
             }
             else
             {
                 bubbles.Stop();
-                transform.localRotation = Quaternion.identity;
-                corkSprite.enabled = true;
-                corkSprite.transform.DOLocalMoveY(0, corkSpeed).SetSpeedBased();
+                vialTransform.localRotation = Quaternion.identity;
+
+                DOTween.Complete(corkSprite);
+                Sequence seq = DOTween.Sequence().SetId(corkSprite);
+                seq.Append(corkSprite.DOFade(1, corkFadeDuration));
+                seq.Append(corkSprite.transform.DOLocalMoveY(0, corkMoveDuration));
             }
         }
 
         public Tween ShowCorrectPrediction()
         {
             Sequence seq = DOTween.Sequence();
-            seq.Join(vialSprite.DOFade(0.0f, tileFadeDuration));
+            seq.Join(glassSprite.DOFade(0.0f, tileFadeDuration));
             
             return seq;
         }
