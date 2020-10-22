44b6ab731a58736caea7ffff04f0a7cf12c60710
Martin Ender <m.buettner.d@gmail.com>
2020-10-22 21:20:54 +0200

    Implement basic score keeper

---
+++ Assets/_Application/Scripts/ScoreKeeper.cs
@@ -0,0 +1,48 @@
+﻿using GMTK2020.Data;
+using System;
+
+namespace GMTK2020
+{
+    public class ScoreKeeper
+    {
+        public int TotalScore { get; private set; }
+
+        private int baseScore;
+
+        private int chainLength;
+
+        public ScoreKeeper(int baseScore)
+        {
+            this.baseScore = baseScore;
+            chainLength = 0;
+
+            TotalScore = 0;
+        }
+
+        public int ScoreStep(SimulationStep step)
+        {
+            int stepScore = 0;
+            switch (step)
+            {
+            case MatchStep matchStep: stepScore = ScoreMatchStep(matchStep); break;
+            case CleanUpStep cleanUpStep: stepScore = ScoreCleanUpStep(cleanUpStep); break;
+            }
+
+            TotalScore += stepScore;
+
+            return stepScore;
+        }
+
+        private int ScoreMatchStep(MatchStep matchStep)
+        {
+            ++chainLength;
+            return matchStep.MatchedTiles.Count * baseScore * chainLength;
+        }
+
+        private int ScoreCleanUpStep(CleanUpStep _)
+        {
+            chainLength = 0;
+            return 0;
+        }
+    }
+}
---
+++ Assets/_Application/Tests/ScoreKeeperTest.cs
@@ -0,0 +1,120 @@
+﻿using GMTK2020;
+using GMTK2020.Data;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Dynamic;
+using UnityEngine;
+
+namespace Tests
+{
+    public class ScoreKeeperTest
+    {
+        [TestCase(10, 3, ExpectedResult = 30)]
+        [TestCase(10, 5, ExpectedResult = 50)]
+        [TestCase(100, 3, ExpectedResult = 300)]
+        public int Basic_match_scores_tiles_times_base_score(int baseScore, int nTiles)
+        {
+            var scoreKeeper = new ScoreKeeper(baseScore);
+
+            var step = CreateMatchStep(nTiles);
+            int matchScore = scoreKeeper.ScoreStep(step);
+
+            return matchScore;
+        }
+
+        [TestCase(10, 3, 2, ExpectedResult = 60)]
+        [TestCase(10, 5, 3, ExpectedResult = 150)]
+        [TestCase(100, 3, 4, ExpectedResult = 1200)]
+        public int Match_chains_escalate_score(int baseScore, int nTiles, int nSteps)
+        {
+            var scoreKeeper = new ScoreKeeper(baseScore);
+
+            for (int i = 0; i < nSteps - 1; ++i)
+                scoreKeeper.ScoreStep(CreateMatchStep(0));
+
+            var step = CreateMatchStep(nTiles);
+            int matchScore = scoreKeeper.ScoreStep(step);
+
+            return matchScore;
+        }
+
+        [Test]
+        public void Clean_up_step_scores_nothing()
+        {
+            var scoreKeeper = new ScoreKeeper(10);
+
+            CleanUpStep step = new CleanUpStep(
+                new List<MovedTile>()
+                {
+                    new MovedTile(),
+                    new MovedTile(),
+                }, 
+                new HashSet<Tile>()
+                {
+                    new Tile(0),
+                    new Tile(1),
+                    new Tile(2),
+                });
+            
+            int cleanUpScore = scoreKeeper.ScoreStep(step);
+
+            Assert.That(cleanUpScore, Is.EqualTo(0));
+        }
+
+        [TestCase(10, 3, 1, ExpectedResult = 30)]
+        [TestCase(10, 3, 2, ExpectedResult = 60)]
+        [TestCase(10, 5, 3, ExpectedResult = 150)]
+        [TestCase(100, 3, 4, ExpectedResult = 1200)]
+        public int Clean_up_step_resets_match_chain(int baseScore, int nTiles, int nSteps)
+        {
+            var scoreKeeper = new ScoreKeeper(baseScore);
+
+            scoreKeeper.ScoreStep(CreateMatchStep(2));
+            scoreKeeper.ScoreStep(CreateMatchStep(3));
+
+            scoreKeeper.ScoreStep(new CleanUpStep(new List<MovedTile>(), new HashSet<Tile>()));
+
+            for (int i = 0; i < nSteps - 1; ++i)
+                scoreKeeper.ScoreStep(CreateMatchStep(0));
+
+            var step = CreateMatchStep(nTiles);
+            int matchScore = scoreKeeper.ScoreStep(step);
+
+            return matchScore;
+        }
+
+        [Test]
+        public void Score_keeper_tracks_total_score()
+        {
+            var scoreKeeper = new ScoreKeeper(10);
+
+            scoreKeeper.ScoreStep(CreateMatchStep(3)); // 3 * 10 * 1 = 30
+            scoreKeeper.ScoreStep(CreateMatchStep(4)); // 4 * 10 * 2 = 80
+
+            scoreKeeper.ScoreStep(CreateCleanUpStep());
+
+            scoreKeeper.ScoreStep(CreateMatchStep(5)); // 5 * 10 * 1 = 50
+            scoreKeeper.ScoreStep(CreateMatchStep(2)); // 2 * 10 * 2 = 40
+
+            scoreKeeper.ScoreStep(CreateCleanUpStep());
+
+            scoreKeeper.ScoreStep(CreateMatchStep(1)); // 1 * 10 * 1 = 10
+
+            Assert.That(scoreKeeper.TotalScore, Is.EqualTo(210));
+        }
+
+        private MatchStep CreateMatchStep(int nMatchedTiles)
+        {
+            var matchedTiles = new HashSet<Tile>();
+            for (int x = 0; x < nMatchedTiles; ++x)
+                matchedTiles.Add(new Tile(0, new Vector2Int(x, 0)));
+
+            return new MatchStep(matchedTiles, new List<MovedTile>());
+        }
+
+        private CleanUpStep CreateCleanUpStep()
+        {
+            return new CleanUpStep(new List<MovedTile>(), new HashSet<Tile>());
+        }
+    }
+}
