664fabad49857d291c9022ae680baf0f0514663e
Martin Ender <m.buettner.d@gmail.com>
2021-01-27 23:43:13 +0100

    WIP: Spawn random molecules

--- Assets/_Application/Scripts/ExtensionMethods.cs
+++ Assets/_Application/Scripts/ExtensionMethods.cs
@@ -131,5 +131,34 @@ namespace GMTK2020
         {
             image.color = new Color(image.color.r, image.color.g, image.color.b, alpha);
         }
+
+        private static int CountCornersVisibleFrom(this RectTransform rectTransform, Camera camera)
+        {
+            Rect screenBounds = new Rect(0f, 0f, Screen.width, Screen.height); // Screen space bounds (assumes camera renders across the entire screen)
+            Vector3[] objectCorners = new Vector3[4];
+            rectTransform.GetWorldCorners(objectCorners);
+
+            int visibleCorners = 0;
+            Vector3 tempScreenSpaceCorner; // Cached
+            for (var i = 0; i < objectCorners.Length; i++) // For each corner in rectTransform
+            {
+                //tempScreenSpaceCorner = camera.WorldToScreenPoint(objectCorners[i]); // Transform world space position of corner to screen space
+                if (screenBounds.Contains(objectCorners[i])) // If the corner is inside the screen
+                {
+                    visibleCorners++;
+                }
+            }
+            return visibleCorners;
+        }
+
+        public static bool IsFullyVisibleFrom(this RectTransform rectTransform, Camera camera)
+        {
+            return CountCornersVisibleFrom(rectTransform, camera) == 4; // True if all 4 corners are visible
+        }
+
+        public static bool IsVisibleFrom(this RectTransform rectTransform, Camera camera)
+        {
+            return CountCornersVisibleFrom(rectTransform, camera) > 0; // True if any corners are visible
+        }
     }
 }
---
+++ Assets/_Application/Scripts/UI/MoleculeSpawner.cs
@@ -0,0 +1,72 @@
+﻿using System.Linq;
+using UnityEngine;
+using Random = System.Random;
+
+namespace GMTK2020.UI
+{
+    [RequireComponent(typeof(RectTransform))]
+    public class MoleculeSpawner : MonoBehaviour
+    {
+        [SerializeField] private float spawnRate = 0.1f;
+        [SerializeField] private float spawnOffset = 50;
+        [SerializeField] private BackgroundMolecule[] moleculePrefabs = null;
+        
+        private RectTransform rectTransform;
+
+        private float lastSpawnTime = 0f;
+        private Random rng;
+        private Vector2 canvasSize;
+        
+        private void Awake()
+        {
+            rectTransform = GetComponent<RectTransform>();
+
+            rng = new Random();
+
+            canvasSize = rectTransform.rect.size;
+        }
+
+        private void Update()
+        {
+            if (Time.time - lastSpawnTime < spawnRate)
+                return;
+
+            BackgroundMolecule molecule = Instantiate(moleculePrefabs[rng.Next(moleculePrefabs.Length)], rectTransform);
+
+            float spawnLocationAlongBorder = (float)(rng.NextDouble() * 2 * (canvasSize.x + canvasSize.y));
+
+            Vector2 spawnPosition;
+            float direction;
+
+            if (spawnLocationAlongBorder < canvasSize.x)
+            {
+                spawnPosition = new Vector2(spawnLocationAlongBorder, -spawnOffset);
+                direction = (float)rng.NextDouble() * 90 + 45;
+            }
+            else if (spawnLocationAlongBorder < canvasSize.x + canvasSize.y)
+            {
+                spawnPosition = new Vector2(canvasSize.x + spawnOffset, spawnLocationAlongBorder);
+                direction = (float)rng.NextDouble() * 90 + 135;
+            }
+            else if (spawnLocationAlongBorder < 2 * canvasSize.x + canvasSize.y)
+            {
+                spawnPosition = new Vector2(spawnLocationAlongBorder, canvasSize.y + spawnOffset);
+                direction = (float)rng.NextDouble() * 90 + 225;
+            }
+            else
+            {
+                spawnPosition = new Vector2(-spawnOffset, spawnLocationAlongBorder);
+                direction = (float)rng.NextDouble() * 90 + 315;
+            }
+
+            Debug.Log(spawnPosition);
+
+            var moleculeTransform = molecule.GetComponent<RectTransform>();
+            moleculeTransform.anchoredPosition = spawnPosition;
+
+            molecule.Direction = new Vector2(Mathf.Cos(direction * Mathf.Deg2Rad), Mathf.Sin(direction * Mathf.Deg2Rad));
+
+            lastSpawnTime = Time.time;
+        }
+    }
+}
