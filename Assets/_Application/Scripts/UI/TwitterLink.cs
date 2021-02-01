db3fa266a813518383349b0cd9c7b835bee24afe
Martin Ender <m.buettner.d@gmail.com>
2021-02-01 22:50:02 +0100

    Make Twitter handles clickable

---
+++ Assets/_Application/Scripts/UI/TwitterLink.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace GMTK2020.UI
+{
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class TwitterLink : MonoBehaviour, IPointerClickHandler
+    {
+        [SerializeField] private string twitterHandle = "";
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            Application.OpenURL($"https://twitter.com/{twitterHandle}");
+        }
+
+        private void Awake()
+        {
+            UpdateLabel();
+        }
+
+        private void OnValidate()
+        {
+            UpdateLabel();
+        }
+
+        private void UpdateLabel()
+        {
+            var text = GetComponent<TextMeshProUGUI>();
+            text.text = $"@{twitterHandle}";
+        }
+    }
+}
