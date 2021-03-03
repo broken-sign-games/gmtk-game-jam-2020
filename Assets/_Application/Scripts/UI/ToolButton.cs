99aff98dda5eb2e824d36517576796bda8ad29b2
Martin Ender <m.buettner.d@gmail.com>
2021-03-03 23:47:41 +0100

    Make tool buttons functional (+ some juice)

--- Assets/_Application/Scripts/UI/ToolButton.cs
+++ Assets/_Application/Scripts/UI/ToolButton.cs
@@ -1,4 +1,5 @@
-﻿using GMTK2020.Audio;
+﻿using DG.Tweening;
+using GMTK2020.Audio;
 using GMTK2020.Data;
 using TMPro;
 using UnityEngine;
@@ -7,23 +8,39 @@ using UnityEngine.UI;
 namespace GMTK2020.UI
 {
     [RequireComponent(typeof(Button))]
+    [RequireComponent(typeof(Image))]
+    [RequireComponent(typeof(RectTransform))]
     public class ToolButton : MonoBehaviour
     {
         [SerializeField] private BoardManipulator boardManipulator = null;
-        [SerializeField] TextMeshProUGUI availableUsesText = null;
-        [SerializeField] TextMeshProUGUI requiredChainLengthText = null;
-        [SerializeField] Tool tool = Tool.SwapTiles;
+        [SerializeField] private TextMeshProUGUI availableUsesText = null;
+        [SerializeField] private Tool tool = Tool.SwapTiles;
+        [SerializeField] private int availableUsesDefaultFontSize = 45;
+        [SerializeField] private int availableUsesUnlimitedFontSize = 60;
+        [SerializeField] private float moveUpBy = 30;
+        [SerializeField] private float overshootBy = 10;
+        [SerializeField] private float moveDuration = 0.5f;
+        [SerializeField] private Sprite defaultSprite = null;
+        [SerializeField] private Sprite activeSprite = null;
 
         protected BoardManipulator BoardManipulator => boardManipulator;
 
         public Tool Tool => tool;
 
+        private RectTransform rectTransform;
         private Button button;
+        private Image buttonImage;
+
+        private float initialYPos;
 
         private void Awake()
         {
+            rectTransform = GetComponent<RectTransform>();
+            buttonImage = GetComponent<Image>();
             button = GetComponent<Button>();
             availableUsesText.text = "0";
+
+            initialYPos = rectTransform.anchoredPosition.y;
         }
 
         public virtual void OnClick()
@@ -35,7 +52,18 @@ namespace GMTK2020.UI
 
         public void UpdateUses(int uses)
         {
-            availableUsesText.text = uses.ToString();
+            if (uses < 0)
+            {
+                availableUsesText.text = "∞";
+                availableUsesText.fontSize = availableUsesUnlimitedFontSize;
+                availableUsesText.fontWeight = FontWeight.Bold;
+            }
+            else
+            {
+                availableUsesText.text = uses.ToString();
+                availableUsesText.fontSize = availableUsesDefaultFontSize;
+                availableUsesText.fontWeight = FontWeight.Regular;
+            }
         }
 
         public void UpdateAvailable(bool available)
@@ -45,17 +73,23 @@ namespace GMTK2020.UI
 
         public void UpdateChainLength(int awarded, int available, int chainLength)
         {
-            
-            requiredChainLengthText.text = $"{awarded}/{available}; x{chainLength}";
         }
 
         public void UpdateActive(bool active)
         {
-            ColorBlock colors = button.colors;
-            Color color = active ? Color.grey : Color.white;
-            colors.normalColor = color;
-            colors.selectedColor = color;
-            button.colors = colors;
+            if (active)
+            {
+                rectTransform
+                    .DOAnchorPosY(initialYPos + moveUpBy, moveDuration)
+                    .SetEase(Ease.OutBack, overshoot: overshootBy);
+            }
+            else
+            {
+                rectTransform
+                    .DOAnchorPosY(initialYPos, moveDuration);
+            }
+
+            buttonImage.sprite = active ? activeSprite : defaultSprite;
         }
     } 
 }
