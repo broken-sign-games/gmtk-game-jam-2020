3a419053c3fcc7fbfcf90d1b78bb97c2ba3d1acf
Martin Ender <m.buettner.d@gmail.com>
2021-10-05 21:09:39 +0200

    Scale chain counter with screen size

---
+++ Assets/_Application/Scripts/UI/ScaleChainCounterWithScreenWidth.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace GMTK2020.UI
+{
+    public class ScaleChainCounterWithScreenWidth : MonoBehaviour
+    {
+        [SerializeField] private float maxWidth = 900f;
+        [SerializeField] private float rightBuffer = 180f;
+
+        private Vector2Int resolution;
+
+        private void Awake()
+        {
+            UpdateCounterWidth();
+        }
+
+#if UNITY_EDITOR
+        private void Update()
+        {
+            if (resolution.x != Screen.width || resolution.y != Screen.height)
+                UpdateCounterWidth();
+        }
+#endif
+
+        private void UpdateCounterWidth()
+        {
+            resolution = new Vector2Int(Screen.width, Screen.height);
+
+            RectTransform parent = transform.parent as RectTransform;
+            float targetWidth = Mathf.Min(maxWidth, parent.rect.width - rightBuffer);
+
+            transform.localScale = new Vector3(targetWidth / maxWidth, targetWidth / maxWidth, 1);
+        }
+    } 
+}
