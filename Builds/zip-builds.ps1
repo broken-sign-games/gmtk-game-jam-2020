c979eae7e509a439b06951b9068ca54dcbaadd65
Martin Ender <m.buettner.d@gmail.com>
2021-10-09 22:43:46 +0200

    Add PowerShell script to zip builds

---
+++ Builds/zip-builds.ps1
@@ -0,0 +1,4 @@
+Compress-Archive -Path alkahest-osx.app -DestinationPath alkahest-osx.app.zip
+Compress-Archive -Path alkahest-webgl -DestinationPath alkahest-webgl.zip
+Compress-Archive -Path alkahest-windows -DestinationPath alkahest-windows.zip
+tar -czvf alkahest-linux.tar.gz alkahest-linux
