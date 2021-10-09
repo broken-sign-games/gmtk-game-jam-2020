104fc074d3f4fb795b59ea3fdba1e89300594ea3
Martin Ender <m.buettner.d@gmail.com>
2021-10-09 22:53:03 +0200

    Update zip script

--- Builds/zip-builds.ps1
+++ Builds/zip-builds.ps1
@@ -1,4 +1,4 @@
-Compress-Archive -Path alkahest-osx.app -DestinationPath alkahest-osx.app.zip
-Compress-Archive -Path alkahest-webgl -DestinationPath alkahest-webgl.zip
-Compress-Archive -Path alkahest-windows -DestinationPath alkahest-windows.zip
+Compress-Archive -Force -Path alkahest-osx.app -DestinationPath alkahest-osx.app.zip
+Compress-Archive -Force -Path alkahest-webgl -DestinationPath alkahest-webgl.zip
+Compress-Archive -Force -Path alkahest-windows -DestinationPath alkahest-windows.zip
 tar -czvf alkahest-linux.tar.gz alkahest-linux
